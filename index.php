ee2aa25626310a75e43e2596414ebb0f7604bcac
melnichukbohdan <melnichukbohdan1@gmail.com>
2021-08-30 14:57:10 +0300

    added db

--- index.php
+++ index.php
@@ -7,3 +7,11 @@ $j = 1;
 echo $b;
 
 echo 'Hello Word!!!';
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=my_library', $user = 'test', $pass = 'BQ#j}v!.1jT$f^');
+    echo 'conected to DB';
+} catch (Exception $e) {
+    echo $e;
+}
+
+
