e100ea41af7a0fdb3c46385047104518a4cc59a5
bogdan <melnichukbohdan1@gmail.com>
2021-09-07 22:38:24 +0300

    added output of current user data in the form of data update

--- phpFiles/updateProfile.php
+++ phpFiles/updateProfile.php
@@ -31,17 +31,17 @@ if  ($_SESSION['message']) {
 <!--sign ip form-->
 <form action="updateUp.php" method="post">
     <label for="login">Username:</label><br>
-    <input type="text" id="login" name="login" minlength="6" maxlength="10" required><br>
+    <input type="text" id="login" name="login" value="<?php echo $_SESSION['user']['login'] ?>", " minlength="6" maxlength="10" required><br>
     <label for="password">Password:</label><br>
     <input type="password" id="password" name="password" minlength="6" maxlength="16"required><br><br>
     <label for="fname">First name:</label><br>
-    <input type="text" id="fname" name="fname" placeholder="Donald*" required><br><br>
+    <input type="text" id="fname" name="fname" value="<?php echo $_SESSION['user']['fname'] ?>" required><br><br>
     <label for="lname">Last name:</label><br>
-    <input type="text" id="lname" name="lname" placeholder="Trump*" required><br><br>
+    <input type="text" id="lname" name="lname" value="<?php echo $_SESSION['user']['lname'] ?>" required><br><br>
     <label for="email">Email</label><br>
-    <input type="text" id="email" name="email" placeholder="email@email" required><br><br>
+    <input type="text" id="email" name="email" value="<?php echo $_SESSION['user']['email'] ?>" required><br><br>
     <label for="phone">Enter your phone number:</label><br>
-    <input type="tel" id="phone" name="phone" placeholder="+3.........." minlength="13" maxlength="13"required><br>
+    <input type="tel" id="phone" name="phone" value="<?php echo $_SESSION['user']['phone'] ?>" minlength="13" maxlength="13"required><br>
 
     <input type="checkbox" id="rules" name="rules" required>
     <label for="rules"><a href="" target="_blank" >I agree to the terms</a></label><br><br>
